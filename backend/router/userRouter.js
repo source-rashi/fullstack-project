7cfbd11bc7d35768b0e83be89aedb5b651b12de3
source-rashi <rashiagrawal1801@gmail.com>
2026-04-22 21:56:00 +0530

    chore: update backend/router/userRouter.js

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   addNewAdmin,
   addNewDoctor,
+  doctorRegister,
   getAllDoctors,
   getUserDetails,
   login,
@@ -19,6 +20,7 @@ import {
 const router = express.Router();
 
 router.post("/patient/register", patientRegister);
+router.post("/doctor/register", doctorRegister);
 router.post("/login", login);
 router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
 router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
@@ -29,5 +31,8 @@ router.get("/admin/me", isAdminAuthenticated, getUserDetails);
 router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
 router.get("/doctor/logout", isDoctorAuthenticated, logoutDoctor);
 router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
+router.post("/patient/logout", isPatientAuthenticated, logoutPatient);
+router.post("/doctor/logout", isDoctorAuthenticated, logoutDoctor);
+router.post("/admin/logout", isAdminAuthenticated, logoutAdmin);
 
 export default router;
