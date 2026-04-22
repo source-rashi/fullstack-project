8413b5f7d3b12b168a0e07ad13b338b29cc0a356
source-rashi <rashiagrawal1801@gmail.com>
2026-04-22 21:55:59 +0530

    chore: update backend/database/dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
-export const dbConnection = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM_DEPLOYED",
-    })
-    .then(() => {
-      console.log("Connected to database!");
-    })
-    .catch((err) => {
-      console.log("Some error occured while connecting to database:", err);
+export const dbConnection = async () => {
+  const dbName =
+    process.env.MONGO_DB_NAME || "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM_DEPLOYED";
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName,
     });
+    console.log(`Connected to database (${dbName})!`);
+  } catch (err) {
+    console.error("Some error occured while connecting to database:", err);
+    throw err;
+  }
 };
